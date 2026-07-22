86889f9b6573fbdde28bf059daf1fe3de91034d4
Arthur Bufon <arthurbufon2052@gmail.com>
2026-07-22 09:49:23 -0300

    Update helpers.php

--- boilerplates/laravel/app/helpers.php
+++ boilerplates/laravel/app/helpers.php
@@ -25,4 +25,60 @@ if (!function_exists('ambienteDev')) {
     }
 }
 
+if (!function_exists('validarCnpjAlfanumerico')) {
+    function validarCnpjAlfanumerico(string $cnpj): bool
+    {
+        $cnpjLimpo = strtoupper($cnpj);
+        $cnpjLimpo = preg_replace('/[^A-Z0-9]/', '', $cnpjLimpo);
+
+        if (! preg_match('/^[A-Z0-9]{12}\d{2}$/', $cnpjLimpo)) {
+            return false;
+        }
+
+        if (preg_match('/^(\d)\1{13}$/', $cnpjLimpo)) {
+            return false;
+        }
+
+        $base = substr($cnpjLimpo, 0, 12);
+        $digitosInformados = substr($cnpjLimpo, 12, 2);
+
+        $pesosPrimeiroDigito = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+        $pesosSegundoDigito = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+
+        $primeiroDigito = calcularDigitoCnpjAlfanumerico($base, $pesosPrimeiroDigito);
+        $segundoDigito  = calcularDigitoCnpjAlfanumerico($base.$primeiroDigito, $pesosSegundoDigito);
+
+        return ($primeiroDigito.$segundoDigito) === $digitosInformados;
+    }
+}
+
+if (!function_exists('calcularDigitoCnpjAlfanumerico')) {
+    function calcularDigitoCnpjAlfanumerico(string $base, array $pesos): int
+    {
+        $soma = 0;
+
+        for ($i = 0; $i < strlen($base); $i++) {
+            $soma += calcularValorCaractereCnpj($base[$i]) * $pesos[$i];
+        }
+
+        $resto = $soma % 11;
+
+        return $resto < 2 ? 0 : 11 - $resto;
+    }
+}
+
+if (!function_exists('calcularValorCaractereCnpj')) {
+    function calcularValorCaractereCnpj(string $caractere): int
+    {
+        return ord($caractere) - 48;
+    }
+}
+
+if (!function_exists('normalizarCnpj')) {
+    function normalizarCnpj(string $cnpj): string
+    {
+        return strtoupper(preg_replace('/[^A-Z0-9]/', '', $cnpj));
+    }
+}
+
 
