d4152461a56b525939ad22682133911561e5baf2
ArthurBufon <arthurbufon2052@gmail.com>
2026-09-07 11:45:26 -0300

    MOLDES: alinhar view service de carros ao contrato

--- moldes/laravel/app/Services/Carro/View/Service.php
+++ moldes/laravel/app/Services/Carro/View/Service.php
@@ -4,25 +4,14 @@ declare(strict_types=1);
 
 namespace App\Services\Carro\View;
 
-use App\Models\Carro;
-
 use App\Queries\Carro\Queries as CarroQueries;
-use App\Queries\Marca\Queries as MarcaQueries;
-use App\Queries\Modelo\Queries as ModeloQueries;
-use App\Queries\Categoria\Queries as CategoriaQueries;
-use App\Queries\Combustivel\Queries as CombustivelQueries;
-use App\Queries\Cambio\Queries as CambioQueries;
 
 class Service
 {
-    public function __construct(
-        private CarroQueries $queries,
-        private MarcaQueries $marcaQueries,
-        private ModeloQueries $modeloQueries,
-        private CategoriaQueries $categoriaQueries,
-        private CombustivelQueries $combustivelQueries,
-        private CambioQueries $cambioQueries,
-    ) {}
+    public function __construct(private CarroQueries $queries)
+    {
+        //
+    }
 
     public function index(array $parametros): array
     {
@@ -51,99 +40,33 @@ class Service
 
     private function dadosIndex(array $parametros): array
     {
-        $filtrosBusca = array_replace(
-            [
-                'empresa_id' => 1,
-                'quantidade' => 100,
-                'ordenacao'  => [
-                    'coluna' => 'id',
-                    'ordem'  => 'desc',
-                ],
-            ],
-            $parametros['filtros'] ?? [],
-        );
-
-        if (ambienteDev()) {
-            // $filtrosBusca['quantidade'] = 10;
-            // $filtrosBusca['marca_id'] = 1;
-        }
-
-        $retornoCarros = $this->queries->index($filtrosBusca)['dados'];
-
-        $carros = $retornoCarros['lista'];
-        $paginacao = $retornoCarros['paginacao'];
-
-        $carros = $carros->map(function (Carro $carro) {
-            return $this->carregarRelacionamentos($carro);
-        });
+        $filtros = $parametros['filtros'] ?? [];
+        $retorno = $this->queries->index($filtros)['dados'];
 
         return [
-            'carros'       => $carros,
-            'paginacao'    => $paginacao,
-            'marcas'       => $this->marcaQueries->index([])['dados']['lista'] ?? [],
-            'modelos'      => $this->modeloQueries->index([])['dados']['lista'] ?? [],
-            'categorias'   => $this->categoriaQueries->index([])['dados']['lista'] ?? [],
-            'combustiveis' => $this->combustivelQueries->index([])['dados']['lista'] ?? [],
-            'cambios'      => $this->cambioQueries->index([])['dados']['lista'] ?? [],
+            'lista'     => $retorno['lista'],
+            'paginacao' => $retorno['paginacao'],
+            'filtros'   => $filtros,
         ];
     }
 
     private function dadosShow(array $parametros): array
     {
-        $carro = $this->carregarRelacionamentos($parametros['carro']);
-
         return [
-            'carro' => $carro,
+            'carro' => $parametros['carro'],
         ];
     }
 
     private function dadosCreate(array $parametros): array
     {
-        return [
-            'marcas'       => $this->marcaQueries->index([])['dados']['lista'] ?? [],
-            'modelos'      => $this->modeloQueries->index([])['dados']['lista'] ?? [],
-            'categorias'   => $this->categoriaQueries->index([])['dados']['lista'] ?? [],
-            'combustiveis' => $this->combustivelQueries->index([])['dados']['lista'] ?? [],
-            'cambios'      => $this->cambioQueries->index([])['dados']['lista'] ?? [],
-        ];
+        return [];
     }
 
     private function dadosEdit(array $parametros): array
     {
-        $carro = $this->carregarRelacionamentos($parametros['carro']);
-
         return [
-            'carro'        => $carro,
-            'marcas'       => $this->marcaQueries->index([])['dados']['lista'] ?? [],
-            'modelos'      => $this->modeloQueries->index([])['dados']['lista'] ?? [],
-            'categorias'   => $this->categoriaQueries->index([])['dados']['lista'] ?? [],
-            'combustiveis' => $this->combustivelQueries->index([])['dados']['lista'] ?? [],
-            'cambios'      => $this->cambioQueries->index([])['dados']['lista'] ?? [],
+            'carro' => $parametros['carro'],
         ];
     }
 
-    private function carregarRelacionamentos(Carro $carro): Carro
-    {
-        $carro->load([
-            'marca',
-            'modelo',
-            'categoria',
-            'combustivel',
-            'cambio',
-
-            'acessorios' => function ($query) {
-                $query->with(['categoria']);
-            },
-
-            'fotos' => function ($query) {
-                $query->orderBy('ordem');
-            },
-
-            'historicoManutencoes' => function ($query) {
-                $query->with(['oficina']);
-            },
-        ]);
-
-        return $carro;
-    }
 }
